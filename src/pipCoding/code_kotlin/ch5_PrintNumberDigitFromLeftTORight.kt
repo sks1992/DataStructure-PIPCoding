7fc555be5343c38f2a98bfe6ebfaed5eef260d63
Sandeep <sandeepsk15892>
2022-03-02 13:27:40 +0530

    code

--- src/pipCoding/code_kotlin/ch5_PrintNumberDigitFromLeftTORight.kt
+++ src/pipCoding/code_kotlin/ch5_PrintNumberDigitFromLeftTORight.kt
@@ -1,5 +1,23 @@
 package pipCoding.code_kotlin
 
-fun main() {
+import java.util.*
+import kotlin.math.pow
 
+fun main() {
+    val scn = Scanner(System.`in`)
+    var n = scn.nextInt()
+    var nod = 0
+    var temp = n
+    while (temp != 0) {
+        temp /= 10
+        nod++
+    }
+    var div = 10.0.pow((nod - 1).toDouble()).toInt()
+    while (div != 0) {
+        val q = n / div
+        println(q)
+        n %= div
+        div /= 10
+    }
 }
+
